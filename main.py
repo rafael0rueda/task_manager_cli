c18d7cb14f2cf1146767203794981255c961e4e3
rafael0rueda <rrrm85@gmail.com>
2025-04-14 15:08:25 -0500

    add task to json

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
-from argparse import ArgumentParser, Namespace
+import json
+import os
+from datetime import datetime
+from argparse import ArgumentParser, Namespace, ArgumentTypeError
+from storage import add_task
 
-parser = ArgumentParser()
-group = parser.add_mutually_exclusive_group()
+def validate_date(date_str):
+    try:
+        return datetime.strptime(date_str, "%Y-%m-%d")
+    except ValueError:
+        raise ArgumentTypeError(f"Ivalid date: {date_str}. Use format YYYY-MM-DD.")
+
+parser = ArgumentParser(description="Task Manager CLI")
 
 parser.usage = '--add "{task}" --due {date} --priority {low | normal | high}'
 
-parser.add_argument('task', type=str, help='The task')
-parser.add_argument('date', type=int, help='The due date of the task')
-parser.add_argument('priority', type=str, help='Priority of the task')
-parser.add_argument('-a','--add', help='Add new task', action="store_true")
-parser.add_argument('-l','--list', help='Show all task', action="store_true")
-parser.add_argument('-d','--done', help='Mark task as completed', action="store_true")
+parser.add_argument('--add', type=str, help='Task description in quotes')
+parser.add_argument('--due', type=validate_date, help='Due date (e.g., 2025-04-15)')
+parser.add_argument('--priority', type=str, choices=['low', 'normal', 'high'], help='Priority: low, normal, or high')
 
 args: Namespace = parser.parse_args()
+file_path = "tasks.json"
 
-print(f"{args.task} due {args.date} with {args.priority}")
+# add a new task require due and priority flags
+if args.add:
+    if not args.due or not args.priority:
+        parser.error("--due and --priority are required when using --add")
+    else:
+          task ={
+                "task": args.add,
+                "due": args.due.strftime("%Y-%m-%d"),
+                "priority": args.priority,
+            }
+          add_task(file_path, task)
--- storage.py
+++ storage.py
@@ -1,1 +1,32 @@
-#  Handles saving - loading tasks
+import os
+import json
+
+def add_task(file_path, new_task):
+    # Load existing data
+        if os.path.exists(file_path):
+            with open(file_path, "r") as file:
+                try:
+                    tasks = json.load(file)
+                except json.JSONDecodeError:
+                    tasks = {}
+        else:
+            tasks = {}
+        
+        # Create new id
+        new_id = len(tasks) + 1
+        new_task_id = f"task{new_id}"
+
+        # Create new tasks
+        data = {
+            "id": new_id,
+            "task": new_task["task"],
+            "due": new_task["due"],
+            "priority":new_task["priority"],
+            "done": False
+        }
+         
+        tasks[new_task_id] = data
+
+        # Save all task back to the file
+        with open(file_path, "w") as file:
+            json.dump(tasks, file, indent=4)
